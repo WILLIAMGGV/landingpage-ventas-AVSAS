010d9d719e4bdf7d8e4baa1285716af537a19cf4
AVSAS <Helperbot@asistentevirtualsas.com>
2024-12-12 15:38:59 -0400

    suiendo completo PC

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import "./App.css";
 import titulo1 from "./img/titulo1.png";
 import titulo2 from "./img/titulo2.png";
@@ -11,9 +12,13 @@ import registrate from "./img/registrate.png"
 import pie from "./img/pie.png"
 
 function App() {
-  
+  const [salir, setSalir] = useState(false);
   
   const guardar = async () => {
+
+    document.getElementById("buttonenviar").innerHTML = "Enviando..."
+    document.getElementById("buttonenviar").disabled = true
+ 
     // Obtener los valores de los campos de entrada
     const data = {
       nombre: document.getElementById("nombre").value,
@@ -42,9 +47,7 @@ function App() {
   
       const result = await response.json();
       console.log(result);
-      document.getElementById("gracias").innerHTML = "Gracias por Registrarte...";
-      document.getElementById("gracias2").innerHTML = "Puedes abrir el documento <a class='text-blue-400 cursor-pointer' href='/matriz.pptx' target='_blank'> AQUI </a> si no abre automáticamente";
-      
+    
       document.getElementById("nombre").value=""
       document.getElementById("apellido").value=""
       document.getElementById("telefono").value=""
@@ -57,11 +60,14 @@ function App() {
     enlace.click(); // Simular un clic en el enlace
     document.body.removeChild(enlace);
     
+    document.getElementById("buttonenviar").innerHTML = "ENVIAR"
+    document.getElementById("buttonenviar").disabled = false
+    setSalir(true)
     
     
     } catch (error) {
       console.error('Error:', error);
-      document.getElementById("gracias").innerHTML = "Ocurrió un error. Por favor, intenta nuevamente. Detalle: " + error.message;
+      //document.getElementById("gracias").innerHTML = "Ocurrió un error. Por favor, intenta nuevamente. Detalle: " + error.message;
     }
   };
   return (
@@ -213,6 +219,7 @@ function App() {
               onClick={() => {
                 guardar();
               }}
+              id="buttonenviar"
               class="text-[#000033] text-[22px] sm:text-[32px] bg-[#00ff99] focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full px-10 py-3 text-center me-2 mb-2"
               style={{ fontFamily: "pextrabold", letterSpacing: "2px" }}
             >
@@ -482,26 +489,85 @@ function App() {
      
       </div>
 
-      <div id="gracias" className="text-[22px]  text-green-500 sm:text-[32px] mt-10 flex flex-row place-content-center" style={{fontFamily: 'pextrabold'}}>
-            
-          </div>
-
-          <div id="gracias2" className="text-[18px] w-[100%] text-blue-800 sm:text-[28px] text-center mt-10" style={{fontFamily: 'pextrabold'}}>
-            
-            </div>
-          
-          
-
+      
       
               <br />
       
       <div className="piedepagina">
 
-        <div style={{ fontFamily: "pextrabold", letterSpacing: "6px", color: 'white' }} className=" pt-[150px] text-[32px] flex flex-row place-content-center items-center justify-center">01.ASISTENTEVIRTUALSAS.COM</div>
+        <div style={{ fontFamily: "pextrabold", letterSpacing: "6px", color: 'white' }} className=" pt-[150px] text-[32px] flex flex-row place-content-center items-center justify-center"><a className='flex flex-row place-content-center' href='https://www.instagram.com/01.asistentevirtual/' target='_blank'><span className='mt-2'><svg class="w-[32px] h-[32px] text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path fill="currentColor" fill-rule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clip-rule="evenodd"/>
+</svg>
+</span><span className='ml-2'>01.ASISTENTEVIRTUALSAS.COM</span></a></div>
       </div>
 
     </div>
     
+
+    {salir ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-lg">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full outline-none focus:outline-none bg-white">
+                {/*body*/}
+                <div className="h-20 bg-[#6633cc] rounded-t-lg place-content-center flex flex-grow text-white text-2xl font-semibold font-mono">
+                  <span className="mt-6 pl-2" style={{ fontFamily: "pextrabold", letterSpacing: "2px" }} >Gracias Por tu Registro</span>
+                </div>
+                <div className="relative p-1 flex-auto">
+                  <div className="p-1 md:p-1 text-center">
+                    <div>
+                      <div className="p-2 md:p-2 font-bold text-center">
+                        
+                      <div id="gracias" className="text-[12px]  text-[#000033] sm:text-[24px] mt-10 flex flex-row place-content-center" style={{fontFamily: 'pextrabold'}}>
+                      ¡Muy pronto nos comunicaremos contigo! Tu plantilla editable para la gestión del tiempo se descargará en un momento.
+            </div>
+  
+            <div id="gracias2" className="text-[12px] w-[100%] text-blue-800 sm:text-[18px] text-center mt-10" style={{fontFamily: 'pextrabold'}}>
+            Puedes abrir el documento <a class='text-blue-400 cursor-pointer' href='/matriz.pptx' target='_blank'> AQUI </a> si no abre automáticamente
+              </div>
+            
+            
+  
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="h-18 bg-[#6633cc] rounded-b-lg place-content-end flex flex-grow text-cyan-300 text-2xl font-semibold font-mono">
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setSalir(false);
+                    }}
+                    data-modal-hide="popup-modal"
+                    class="m-2 text-gray-900 bg-[#2ae69a] hover:bg-[#00ff99]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 mb-2"
+                  >
+                    <svg
+                      class="w-6 h-6 text-gray-800 "
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm7.707-3.707a1 1 0 0 0-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 1 0 1.414 1.414L12 13.414l2.293 2.293a1 1 0 0 0 1.414-1.414L13.414 12l2.293-2.293a1 1 0 0 0-1.414-1.414L12 10.586 9.707 8.293Z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                    &nbsp;Cerrar
+                  </button>
+               
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
 
   
     </>
